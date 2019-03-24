447b4e5959cc8dbe71ab560a4d11c97932205767
gizmore <gizmore@wechall.net>
2019-03-24 23:55:29 +0100

    6.09-r101_phpinfo

--- tpl/navbar.php
+++ tpl/navbar.php
@@ -4,7 +4,6 @@ use GDO\UI\GDT_Link;
 use GDO\Core\GDT_Hook;
 $bar = GDT_Bar::make('admintabs')->horizontal();
 $bar->addFields(array(
-	GDT_Link::make('btn_phpinfo')->href(href('Core', 'PHPInfo')),
 	GDT_Link::make('btn_clearcache')->href(href('Admin', 'ClearCache')),
 	GDT_Link::make('btn_modules')->href(href('Admin', 'Modules')),
 	GDT_Link::make('btn_users')->href(href('Admin', 'Users')),
