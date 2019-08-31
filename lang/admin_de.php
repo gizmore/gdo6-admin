c5cf91f594322edcf6e1eb2c2cfb9b9bc3e34c78
gizmore <gizmore@wechall.net>
2019-08-31 16:23:06 +0200

    6.10-r42_german_lang

---
+++ lang/admin_de.php
@@ -0,0 +1,59 @@
+<?php
+return array(
+'btn_admin' => 'Admin',
+'btn_phpinfo' => 'PHPInfo',
+'btn_clearcache' => 'Cache leeren',
+'btn_modules' => 'Module',
+'btn_users' => 'Nutzer',
+'btn_permissions' => 'Berechtigungen',
+'btn_cronjob' => 'Hintergrunddienste',
+'btn_login_as' => 'Einloggen al…',
+'btn_reinstall' => 'Neu Installieren',
+################################################################
+'err_module' => 'Unbekanntes Modul.',
+'version_db' => 'Vers.',
+'version_fs' => 'V.FS.',
+'btn_install' => 'Installieren',
+'btn_configure' => 'Configurieren',
+'btn_uninstall' => 'Löschen',
+'btn_enable' => 'Aktivieren',
+'btn_disable' => 'Deaktivieren',
+'msg_module_enabled' => 'Das %s Modul wurde aktiviert.',
+'msg_module_disabled' => 'Das %s Modul wurde deaktiviert.',
+################################################################		
+'ft_admin_install' => '%s installieren',
+'msg_module_installed' => 'Das %s Modul wurde erfolgreich installiert.',
+'msg_module_uninstalled' => 'Das %s Modul wurde deinstalliert. Alle seine Tabellen wurden gelöscht',
+################################################################
+'ft_admin_configure' => '%s konfigurieren',
+'module_version' => 'Version',
+'version_available' => 'Version verfügbar',
+'form_div_config_vars' => 'Konfiguration',
+'msg_module_saved' => 'Konfiguration wurde gespeichtert. %s',
+'msg_modulevar_changed' => '%s wurde von <i>%s</i> nach <i>%s</i> geändert.',
+'href_admin' => 'Admin Bereich',
+'confirm_wipe_module' => 'Möchten Sie diese Datenbank-Tabellen löschen: %s?',
+################################################################
+'ft_admin_useredit' => 'Nutzer ´%s´ editieren',
+'msg_user_password_is_now' => 'Das Passwort dieses Nutzers lautet nun: %s',
+################################################################
+'link_add_perm' => 'Neue Berechtigung',
+'link_grant_perm' => 'Berechtigung zuweisen',
+'link_edit_permissions' => 'Berechtigungen bearbeiten',
+'ft_admin_permissionadd' => 'Berechtigung hinzufügen',
+'user_count' => 'Mitglieder',
+'perm_revoke' => 'Berechtigung zurückweisen',
+'msg_perm_added' => 'Eine neue Berechtigung wurde zugewiesen %s',
+################################################################
+'ft_admin_permissionrevoke' => 'Berechtigung zurückweisen',
+################################################################
+'ft_admin_permissiongrant' => 'Berechtigung gewähren',
+'choose_permission' => 'Berechtigung auswählen',
+'msg_perm_granted' => 'Sie haben die Berechtigung %s dem Nutzer %s zugewiesen.',
+################################################################
+'msg_cache_flushed' => 'Alle Caches wurden gelöscht. Z.B. rm -rf temp/minify, Cache::flush().',
+################################################################
+'link_create_user' => 'Neuen Nutzer anlegen',
+'ft_admin_usercreate' => 'Neuen Nutzer anlegen',
+################################################################
+);
