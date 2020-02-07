ff9c14e9a1b3c3fb09faa444e795efdc8fa19330
gizmore <gizmore@wechall.net>
2020-02-07 14:33:45 +0100

    6.10-r5020_language_in_js

--- Method/ClearCache.php
+++ Method/ClearCache.php
@@ -1,14 +1,21 @@
 <?php
 namespace GDO\Admin\Method;
 
-use GDO\Core\GDO;
 use GDO\Core\MethodAdmin;
 use GDO\Core\GDT_Hook;
 use GDO\Core\Method;
 use GDO\DB\Cache;
 use GDO\File\FileUtil;
 use GDO\Util\MinifyJS;
+use GDO\Core\Module_Core;
 
+/**
+ * Clears all client and server caches.
+ * 
+ * @author gizmore
+ * @version 6.10
+ * @since 6.01
+ */
 final class ClearCache extends Method
 {
 	use MethodAdmin;
@@ -17,9 +24,18 @@ final class ClearCache extends Method
 	
 	public function execute()
 	{
+		# Flush memcached and gdo cache.
 		Cache::flush();
+		# Call hook
 		GDT_Hook::callWithIPC('ClearCache');
+		# Remove minified JS
 		FileUtil::removeDir(MinifyJS::tempDirS());
+		# Retrigger assets
+		$core = Module_Core::instance();
+		$assetVersion = $core->cfgAssetVersion() + 0.01;
+		$core->saveConfigVar('asset_revision', $assetVersion);
+		# Done
 		return $this->renderNavBar()->add($this->message('msg_cache_flushed'));
 	}
+
 }
